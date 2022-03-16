b381a8d17b6fd89292dfaf44385c594a88ec6370
Yara Elmhamid <yelmhamid@anrpcc.corp>
2022-03-16 14:55:12 +0100

    basic types with TS

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,21 @@
-var firstName = 'yara';
-console.log(firstName);
+/*lesson #2 */
+//const firstName = 'yara';
+//console.log(firstName)
 //To obtain a NodeList of all of the <input> elements contained within the element "document"
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+//const inputs = document.querySelectorAll('input')
+//console.log(inputs)
+//inputs.forEach(input => {
+// console.log(input)
+//})
+/*lesson #3 */
+var firstName = "yara";
+var age = 23;
+var loveCat = true;
+//firstName=50;
+firstName = "lamia";
+age = 25;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+//console.log(circ("yara"))
+console.log(circ(5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,23 @@
-const firstName = 'yara';
-console.log(firstName)
+/*lesson #2 */
+//const firstName = 'yara';
+//console.log(firstName)
 
 //To obtain a NodeList of all of the <input> elements contained within the element "document"
-const inputs = document.querySelectorAll('input')
-console.log(inputs)
+//const inputs = document.querySelectorAll('input')
+//console.log(inputs)
 
-inputs.forEach(input => {
-    console.log(input)
-})
+//inputs.forEach(input => {
+   // console.log(input)
+//})
+/*lesson #3 */
+let firstName = "yara";
+let age = 23;
+let loveCat = true;
+//firstName=50;
+firstName = "lamia";
+age = 25;
+const circ = (diameter: number)=>{
+    return diameter*Math.PI;
+}
+//console.log(circ("yara"))
+console.log(circ(5));
